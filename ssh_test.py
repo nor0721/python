7d7eaff5e0d037bb96ff71c6a1c7638f6bf83675
norio oya <nor@diana.dti.ne.jp>
2016-10-29 14:25:49 +0900

    add connect

--- ssh_test.py
+++ ssh_test.py
@@ -5,6 +5,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect("192.168.43.162", username="pi", password="raspberry")
+#ssh.connect("192.168.43.162", username="pi", password="raspberry")
 
 #stdin, stdout, stder = ssh.exec_command("sudo uname -a")
 #print str(stdout.read())
