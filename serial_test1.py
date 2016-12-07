4fc630745e6fdf63c58ba4b938f94dcf808866e6
norio oya <nor@diana.dti.ne.jp>
2016-12-07 19:17:06 +0900

    rubyでの開発に方針変更

--- serial_test1.py
+++ serial_test1.py
@@ -7,15 +7,15 @@ time.sleep(2)
 
 while True:
     var = raw_input()
-    print("input>>" +var)
+    print("input>>" + var)
     if var == "x":
-        break
-    if var =="a":
+        reak
+    if var == "a":
         ser.write(b'a')
-        print("A")
-    if var =="b":
+        rint("A")
+    if var == "b":
         ser.write(b'B')
-        print("B")
+       print("B")
 print("end")
 ser.close()
 
